8fe6d0302fcda60facea65592ea49fa66282dde5
Mrugesh Panchal <mrugesh@weisetech.com>
2025-06-03 15:48:57 +0530

    New class added

--- AG/src/main/java/utils/ExtentReportManager.java
+++ AG/src/main/java/utils/ExtentReportManager.java
@@ -2,6 +2,7 @@ package utils;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.observer.ExtentObserver;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
@@ -9,28 +10,35 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class ExtentReportManager {
     private static ExtentReports extent;
-    // protected ExtentTest test;
-    //  private static File TakesScreenshot;
+     protected ExtentTest test;
+      private static File TakesScreenshot;
 
     public static ExtentReports getReportInstance() {
-        if (extent == null) {
-            ExtentSparkReporter sparkReporter = new ExtentSparkReporter("reports/ExtentReport.html");
-            sparkReporter.config().setReportName("Automation Test Report");
-            sparkReporter.config().setDocumentTitle("Image Count Test");
+        if (extent == null)
+        {
+            String timestamp = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(new Date());
+            String reportPath = "reports/ExtentReport_" + timestamp + ".html";
+            ExtentSparkReporter reporter = new ExtentSparkReporter(reportPath);
+
+            reporter.config().setDocumentTitle("Automation Test Report");
+            reporter.config().setReportName("Test Output Report");
 
             extent = new ExtentReports();
-            extent.attachReporter(sparkReporter);
+            ExtentObserver Reporter = null;
+            extent.attachReporter(Reporter);
         }
         return extent;
     }
 
     public static <string> ExtentTest createTest(string testName) {
 
-        ExtentTest test = getReportInstance().createTest("String.valueOf(testName)");
-        return test;
+        return getReportInstance().createTest("String.valueOf(testName)");
     }
 
     public static String captureScreenshot(WebDriver driver, String scrrenshotName) {
